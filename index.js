e374c4641be09c1f795732683d72945ca8434fc9
Sadullah-TANRIKULU <sadullahtanrikulu@gmail.com>
2024-06-25 02:01:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cron = require('node-cron');
+
+const app = express();
+const port = 3000;
+
+const websiteUrl = 'https://weekly-automated-reading-job-2cfbf.web.app';
+
+const triggerWebsite = async () => {
+  try {
+    const response = await fetch(websiteUrl);
+    const data = await response.text();
+    console.log(response);
+    console.log(`Website triggered: ${data}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+cron.schedule('10 11 1 * * *', () => {
+
+    triggerWebsite();
+    console.log("website is triggered");
+//   }
+});
+
+console.log('Script is running...');
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
